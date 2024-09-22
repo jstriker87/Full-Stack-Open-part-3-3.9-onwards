1d8a769f17bab97b5188fb4302d0746fea06fc98
James Bart <jbart1@gmail.com>
2024-09-22 01:40:45 +0100

    Completed exercise 3.12

--- mongo.js
+++ mongo.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
+
+if (process.argv.length < 4) {
+  Person.find({}).then(result => {
+  result.forEach(person => {
+    console.log(person)
+  })
+    mongoose.connection.close()
+  })
+}
+
 
 const password = process.argv[2]
 
@@ -19,13 +29,22 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-const addPerson = (content, important) => {
+const addPerson = (name, number) => {
   const person = new Person({
-    content,
-    important,
+    name,
+    number,
   })
   return person.save()
 }
+
+let regex = /^[a-zA-Z]+$/;
+
+if (regex.test(process.argv[4]) == true){
+    console.log('The Phone number provided contains alpha numeric characters. If the persons name contains a space it must be entered within quoation marks')
+    process.exit(1)
+}
+
+
 
 addPerson(process.argv[3], process.argv[4]).then(result => {
     console.log(result)
