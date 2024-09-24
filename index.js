e485a4008f39896aeb33b0898deb3f7a22d5460a
James Bart <jbart1@gmail.com>
2024-09-24 20:20:38 +0100

    Completed exercises 3.15 & 3.16

--- index.js
+++ index.js
@@ -29,26 +29,29 @@ app.get('/', (request, response) => {
 //    response.send(`Phonebook has info for ${persons.length} notes </br> ${currentDateString}`); 
 //})
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(notes => {
     response.json(notes)
   })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   id = request.params.id
   Person.findById(request.params.id).then(person=>{
     response.json(person)
   })
+    .catch(error => next(error))
 })
 
-
-app.delete('/api/persons/:id', (request, response) => {
-    id = request.params.id
-    Person.findByIdAndDelete(id).then(person=> {
-        response.json(person)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
+    .catch(error => next(error))
 })
+
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -71,8 +74,29 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
