0b13388d4fa7eb5f912d1df346072e714da0f096
James Bart <jbart1@gmail.com>
2024-09-24 01:14:04 +0100

    Working through further part 3 examples

---
+++ examples/error-handling-and-moving-error-handling-to-middleware/index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.static('dist'))
+const cors = require('cors')
+app.use(cors({ origin: 'http://localhost:5173' }));
+const morgan = require('morgan')
+require('dotenv').config();
+
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :response-time ms :body'))
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI;
+console.log(url)
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const Note = require('./models/note')
+
+var currentDate = new Date(); 
+var currentDateString = currentDate.toString();
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+//app.get('/info', (request, response) => {
+//    response.send(`Phonebook has info for ${notes.length} notes </br> ${currentDateString}`); 
+//})
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end() 
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    Note.findById(request.params.id).then(note => {
+       response.json(note)
+  })
+})
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    }
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    })
+
+    //const duplicate = notes.find(notecheck =>notecheck.name ==note.content)
+    //if (duplicate) {
+    //    return response.status(400).json({ 
+    //        error: `The name ${note.name} already exists` 
+    //    })
+    //}
+
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
+})
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
